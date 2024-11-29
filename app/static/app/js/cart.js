f61322b93ac9d5c59d1f07c04968ca14b24e071a
levansy <levansy02@gmail.com>
2024-11-29 09:33:21 +0700

    6. update lan 6

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -6,9 +6,28 @@ for (i = 0; i < updateBtns.length; i++) {
     console.log("product.Id", productId, "action", action);
     console.log("user: ", user);
     if (user === "AnonymousUser") {
-      console.log("user not logged in");
+      console.log("user not logged");
     } else {
-      console.log("user logged in, success add");
+      updateUserOrder(productId, action);
     }
   });
+}
+
+function updateUserOrder(productId, action) {
+  console.log("user logged in, success add");
+  var url = "update_item/";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": "csrftoken",
+    },
+    body: JSON.stringify({ productId: productId, action: action }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data", data);
+    });
 }
